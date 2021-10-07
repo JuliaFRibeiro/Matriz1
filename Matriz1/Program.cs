e978ddf38b8e81a1fd65ec790a5936e4553f7846
Julia <julia_felix03@hotmail.com>
2021-10-06 22:05:33 -0300

    Adicionar arquivos de projeto.

---
+++ Matriz1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarando variavel
+            int n = int.Parse(Console.ReadLine());
+
+            //tamanho da matriz dada pelo usuario
+            int[,] mat = new int[n, n];
+
+            //loop linha
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                //loop coluna
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            //gera o resultado
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            //formula da matriz
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    //condição
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            //gera o resultado
+            Console.WriteLine("Negative numbers: " + count);
+
+            Console.ReadKey();
+        }
+    }
+}
